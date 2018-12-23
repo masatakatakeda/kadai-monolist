b34a360f810d56d2e56ba6a7cce7aee6bae1bdcc
masa.nosuke <ezk04056@yahoo.co.jp>
2018-12-23 04:59:53 +0000

    have

--- routes/web.php
+++ routes/web.php
@@ -22,9 +22,6 @@ Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
-Route::group(['middleware' => ['auth']], function () {
-    Route::resource('items', 'ItemsController', ['only' => ['create']]);
-});
 
 Route::group(['middleware' => ['auth']], function () {
     Route::resource('items', 'ItemsController', ['only' => ['create', 'show']]);
@@ -33,3 +30,9 @@ Route::group(['middleware' => ['auth']], function () {
     Route::resource('users', 'UsersController', ['only' => ['show']]);
 });
 
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('items', 'ItemsController', ['only' => ['create', 'show']]);
+    Route::post('have', 'ItemUserController@have')->name('item_user.have');
+    Route::delete('have', 'ItemUserController@dont_have')->name('item_user.dont_have');
+    Route::resource('users', 'UsersController', ['only' => ['show']]);
+});
